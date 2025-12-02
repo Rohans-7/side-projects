403cfdf30e1d5155c86ea803dc4dd99c72d487a9
Rohan Sonawane <rohansonawane1818@gmail.com>
2025-12-03 00:52:54 +0530

    Read input through inputstream

--- distributed-key-value/Server.java
+++ distributed-key-value/Server.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 public class Server {
-
+    private static ConcurrentHashMap<Integer, Integer> keyValueStore = new ConcurrentHashMap<>();
     public static void main(String[] args){
         System.out.println("Server is running...");
         ServerSocket serverSocket = new ServerSocket(5000);
@@ -17,7 +17,29 @@ public class Server {
     }
     public static void handleClient(Socket socket){
         try{
-            
+            BufferedReader data = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+
+            String line;
+            while((line = data.readLine()) != null){
+                String parts[] = line.split(" ");
+                String command = parts[0];
+                switch(command.toUpperCase()){
+                    case "PUT":
+                        int key = Integer.parseInt(parts[1]);
+                        int value = Integer.parseInt(parts[2]);
+                        keyValueStore.put(key, value);
+                        pw.println("Stored");
+                        break;
+                    case "GET":
+                        key = Integer.parseInt(parts[1]);
+                        Integer val = keyValueStore.get(key);
+                        pw.println(val == null ? "NULL" : val);
+                        break;
+                }
+                pw.println(response);
+            }
         }
     }
+
 }
